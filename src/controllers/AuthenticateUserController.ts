b6bda65a5332e335dbe35400f3e8206c92ab1dbf
Jeferson Mesquita <je_2742@hotmail.com>
2021-10-19 21:18:43 -0300

    51 min
    51 min

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,19 @@
+
+import {Request, Response} from 'express'
+import { AuthenticateUserService } from '../services/AuthenticateUserService'
+
+class AuthenticateUserController {
+    async handle(req: Request, res: Response){
+        const { code } = req.body
+        const service = new AuthenticateUserService()
+        if(!code){
+            return res.status(401)
+        }
+
+        const result = await service.execute(code)
+        
+        return res.status(200).json(result)
+    }
+}
+
+export {AuthenticateUserController}
